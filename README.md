602cad6d0b15e9c249f70e00c43ec27464abee71
Juris Ratnieks <ratnieks90@inbox.lv>
2020-07-02 14:57:02 +0300

    updated README.md and added tests for employeeService.js

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Requirements - Node 12.16^, npm 6.14^
 1. npm install -> install all dependencies
 2. npm run build -> build project public part
 3. npm run start -> start node server to work with API.
+4. npm run test -> run tests
--- public/js/employeeService.test.js
+++ public/js/employeeService.test.js
@@ -107,7 +107,6 @@ describe('Employee service add employee: ', () => {
 
     beforeEach(() => {
         employee = {
-            "id": 1,
             "name": "John1",
             "surname": "Doe1",
             "email": "johnDoe1@gmail.com",
@@ -167,6 +166,157 @@ describe('Employee service add employee: ', () => {
         axios.get.mockRejectedValue(errorResponse);
 
         return employeeService.addEmployee().catch(resp => {
+            expect(resp).toBe('something went wrong');
+        })
+    })
+});
+
+describe('Employee service edit employee: ', () => {
+    let response;
+    let employee;
+    let message;
+    let errorResponse;
+
+    beforeEach(() => {
+        employee = {
+            "name": "John1",
+            "surname": "Doe1",
+            "email": "johnDoe1@gmail.com",
+            "phone": 28365031,
+            "description": "test description"
+        };
+        message = `Employee ${employee.name} ${employee.surname} successfully updated`;
+        response = {
+            data: {
+                message: message,
+                data: employee,
+                changes: 1
+            }
+        };
+        errorResponse = {
+            response: {
+                data: {
+                    error: 'something went wrong'
+                }
+            }
+        }
+    });
+
+    test('should return response object', () => {
+        axios.put.mockResolvedValue(response);
+
+        return employeeService.editEmployee(1, employee).then(response => {
+            expect(response).toBeDefined();
+        })
+    })
+
+    test('should contain message key', () => {
+        axios.put.mockResolvedValue(response);
+
+        return employeeService.editEmployee(1, employee).then(response => {
+            expect(response.message).toBeDefined();
+        })
+    })
+
+    test('should contain changes key', () => {
+        axios.put.mockResolvedValue(response);
+
+        return employeeService.editEmployee(1, employee).then(response => {
+            expect(response.changes).toBeDefined();
+        })
+    })
+
+    test('should changes key have value 1', () => {
+        axios.put.mockResolvedValue(response);
+
+        return employeeService.editEmployee(1, employee).then(response => {
+            expect(response.changes).toEqual(1);
+        })
+    })
+
+    test('should response contain data', () => {
+        axios.put.mockResolvedValue(response);
+
+        return employeeService.editEmployee(1, employee).then(response => {
+            expect(response.data).toBeDefined();
+        })
+    })
+
+    test('should response data contain employee object', () => {
+        axios.put.mockResolvedValue(response);
+
+        return employeeService.editEmployee(1, employee).then(response => {
+            expect(response.data).toEqual(expect.objectContaining(employee));
+        })
+    })
+
+    test('should catch contain error string', () => {
+        axios.put.mockRejectedValue(errorResponse);
+
+        return employeeService.editEmployee(1, employee).catch(resp => {
+            expect(resp).toBe('something went wrong');
+        })
+    })
+});
+
+describe('Employee service delete employee: ', () => {
+    let response;
+    let message;
+    let errorResponse;
+
+    beforeEach(() => {
+        message = 'Employee successfully deleted';
+        response = {
+            data: {
+                message: message,
+                changes: 1
+            }
+        };
+        errorResponse = {
+            response: {
+                data: {
+                    error: 'something went wrong'
+                }
+            }
+        }
+    });
+
+    test('should return response object', () => {
+        axios.delete.mockResolvedValue(response);
+
+        return employeeService.deleteEmployee(1).then(response => {
+            expect(response).toBeDefined();
+        })
+    })
+
+    test('should contain message key', () => {
+        axios.delete.mockResolvedValue(response);
+
+        return employeeService.deleteEmployee(1).then(response => {
+            expect(response.message).toBeDefined();
+        })
+    })
+
+    test('should contain changes key', () => {
+        axios.delete.mockResolvedValue(response);
+
+        return employeeService.deleteEmployee(1).then(response => {
+            expect(response.changes).toBeDefined();
+        })
+    })
+
+    test('should changes key have value 1', () => {
+        axios.delete.mockResolvedValue(response);
+
+        return employeeService.deleteEmployee(1).then(response => {
+            expect(response.changes).toEqual(1);
+        })
+    })
+
+    test('should catch contain error string', () => {
+        axios.delete.mockRejectedValue(errorResponse);
+
+        return employeeService.deleteEmployee(1).catch(resp => {
             expect(resp).toBe('something went wrong');
         })
     })
